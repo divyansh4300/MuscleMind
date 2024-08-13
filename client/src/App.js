fec2ac905a774ea1e9b5b21d8f7e9d177b5e7d6d
Dkay <159882022+divyansh4300@users.noreply.github.com>
2024-08-13 14:06:48 +0530

    3 implement sidebar (#4)
    * Implemented sidebar
    
    * Update styles.module.css

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,24 @@ import Main from "../src/components/Main/index";
 import Signup from "../src/components/Signup/index";
 import Login from "../src/components/Login/index";
 
-
 function App() {
-	const user = localStorage.getItem("token");
+  const user = localStorage.getItem("token");
 
-	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      {user ? (
+        <>
+          <Route path="/*" element={<Main />} />
+        </>
+      ) : (
+        <>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate replace to="/login" />} />
+        </>
+      )}
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/components/tabs/AddTrainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddTrainer = () => {
+  return (
+    <div>
+      <h1>AddTrainer</h1>
+    </div>
+  );
+};
+
+export default AddTrainer;
